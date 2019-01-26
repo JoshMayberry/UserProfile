a6b133b99945d96019877cae5df2e0312202af70
HOME\JoshuaMayberry <joshua.mayberry1991@gmail.com>
2019-01-26 10:54:08 -0500

    https://classroom.udacity.com/nanodegrees/nd803/parts/686861db-d0bf-4fe2-ab57-287444412d3f/modules/434cf327-f998-4e1d-8d7a-04ed96c43872/lessons/dc8f22de-9fe6-4e1a-b570-fe9c683909ed/concepts/2121c280-4641-46ce-bb97-2f055f794433#

--- app/src/main/java/com/example/android/userprofile/MainActivity.java
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.android.userprofile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView view_name;
+    TextView view_country;
+    TextView view_birthday;
+    ImageView view_pofilePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view_name = findViewById(R.id.name);
+        view_country = findViewById(R.id.country);
+        view_birthday = findViewById(R.id.birthday);
+        view_pofilePicture = findViewById(R.id.profile_picture);
+
+        view_name.setText("Kade");
+        view_country.setText("USA");
+        view_birthday.setText("1991");
+        view_pofilePicture.setImageResource(R.drawable.lyla);
+
     }
 }
